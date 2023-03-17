3f62c503da1c5dc54b8d7e604dc6009c16ae13d4
Anushri Daryapurkar <98867361+Anushri-Daryapurkar@users.noreply.github.com>
2023-03-17 17:00:48 +0530

    Add files via upload

---
+++ Logic_Building/Assignment2/Pattern2.java
@@ -0,0 +1,27 @@
+class Pattern2
+{
+	public static void main(String args[])
+	{
+		for(char i = 'A'; i<='E'; i++)
+		{
+			for(char j = 'A'; j<=i; j++)
+			{
+				System.out.print(j + " ");
+			}
+			System.out.println();
+		}
+		
+		/*
+		for(int i=65; i<=69; i++)
+		{
+			for(int j=65; j<=i; j++)
+			{
+				System.out.print((char)j + " ");
+			}
+			System.out.println();
+		}
+		*/
+	
+	
+	}
+}
