b44d26bafe7a8046cf852cb4df37872f68887576
Anushri Daryapurkar <98867361+Anushri-Daryapurkar@users.noreply.github.com>
2023-03-22 18:22:09 +0530

    Add files via upload

---
+++ Concepts of Programming/Assignment2/Pattern11.java
@@ -0,0 +1,24 @@
+class Pattern11
+{
+	public static void main(String args[])
+	{
+		for(int i=5; i>=1; i--)
+		{
+			for(int j = 1; j<=i; j++)
+			{
+				System.out.print(" ");
+			}
+			for(int k=5; k>=i; k--)
+			{
+				System.out.print("*");
+			}
+			for(int k1=i; k1<=4; k1++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+	
+	
+}
